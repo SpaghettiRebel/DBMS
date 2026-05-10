edb44573ffdbfcca1dfbbe2eeb6563c88fa9996a
SpaghettiRebel <slavadem3106@gmail.com>
2026-05-11 02:21:44 +0300

    ass container + pp alloc

--- dbms_project/server/storage/include/associative_container.h
+++ dbms_project/server/storage/include/associative_container.h
@@ -1,2 +1,17 @@
-#pragma once
-// Basic definitions for associative containers if needed
+#ifndef ASSOCIATIVE_CONTAINER_H
+#define ASSOCIATIVE_CONTAINER_H
+
+#include <iostream>
+#include <vector>
+
+template <typename compare, typename tkey>
+concept comparator = requires(const compare c, const tkey& lhs, const tkey& rhs) {
+    { c(lhs, rhs) } -> std::same_as<bool>;
+} && std::copyable<compare> && std::default_initializable<compare>;
+
+template <typename f_iter, typename tkey, typename tval>
+concept input_iterator_for_pair =
+    std::input_iterator<f_iter> &&
+    std::same_as<typename std::iterator_traits<f_iter>::value_type, std::pair<tkey, tval>>;
+
+#endif
