fcc28ee3d45dc45f2c7b2a1063fe7136fe122f73
Heavenfiend <EGS-76@yandex.ru>
2026-05-13 12:00:11 +0300

    feat: implement interactive REPL client and fix CMake structure

--- dbms_project/client/main.cpp
+++ dbms_project/client/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char* argv[]) {
-    std::cout << "DBMS Client stub" << std::endl;
+    std::string command;
+
+    while (true) {
+        std::cout << "dbms> " << std::flush;
+
+        if (!std::getline(std::cin, command)) {
+            break;
+        }
+
+        if (command == "exit") {
+            break;
+        }
+
+        std::cout << "Executing..." << std::endl;
+    }
+
     return 0;
 }
